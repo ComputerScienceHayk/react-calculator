6cc1661f53624dcd8ed3f5ebc2afa0a1fe20790c
ComputerScienceHayk <59318612+ComputerScienceHayk@users.noreply.github.com>
2020-06-25 22:50:16 +0400

    Updates

---
+++ precache-manifest.0b258783eaf6b0dacd8c7294515a8f8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f6a9ec647ef00e47f71bb8dd0ae441",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "30429d7f9e2071156609",
+    "url": "/react-calculator/static/css/main.53858a3e.chunk.css"
+  },
+  {
+    "revision": "9fd600fa46a798a23830",
+    "url": "/react-calculator/static/js/2.72b7087d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.72b7087d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30429d7f9e2071156609",
+    "url": "/react-calculator/static/js/main.ce8b7497.chunk.js"
+  },
+  {
+    "revision": "c0af51cd212b8448c260",
+    "url": "/react-calculator/static/js/runtime-main.979a65f7.js"
+  }
+]);
